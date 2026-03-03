776b656af81fb574100f07dfd7d3f5cb1806bb1b
Brandon-1606 <blizardel2400@alumno.ipn.mx>
2026-03-02 18:07:19 -0600

    Mi primer hola mundo

---
+++ HolaMundo01.java
@@ -0,0 +1,15 @@
+/*
+Java es un lenguaje de programación multi paradigma, significa que se adapta a las necesidades del modelo o arquitectura de programacióm.
+
+Java originalmente fue pensado para programar electrodomesticos, este lenguaje utiliza una maquina virtual de java. Esa maquina virtual se encarga de transformar el código de maquina 01 en las instrucciones definidas por el programador y vceversa. Cuando programamos en JAVA debemos de compilar el código.
+ */
+
+class HolaMundo01{
+    //Este es el cuerpo de la clase
+    //Aqui se definen las variables globales
+
+    //Aqui definimos los comportamientos(métodos)
+    public static void main(String[] args){
+        System.out.print("Hola mundo mundial");
+    }
+}
